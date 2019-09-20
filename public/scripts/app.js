5cdc68ec7e365bf9cdcc6878dec3a29c345d3ec9
Michael-Choi <Michael.choi@queensu.ca>
2019-09-20 20:42:06 +0000

    added helper functions to add load resources in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,5 +12,52 @@ $(() => {
 });
 
 $(document).ready(() => {
-  loadTweets();
+  loadResources();
 });
+
+async function loadResources() {
+  try {
+    await $.ajax({
+      url: "http://localhost:8080/:category",
+      dataType: "JSON",
+      success: data => {
+        renderResource(data);
+      }
+    });
+
+    // .then(data => renderTweets(data));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//! THIS NEEDS TO BE STYLED AND FORMATTED ACCORDING TO UI FRAMEWORK
+function createResourceElement(resourceData) {
+  console.log(resourceData);
+  const resource = `
+  <section class="resources">
+    <div class="resourceImg">
+      <img href="${escape(resourceData.___)}"></img>
+    </div>
+    <div class='textbody'>
+      <p>Title</p>
+      <form>
+          <button class="favorite styled" value="upvote" type="button">
+            UPVOTE
+          </button>
+          <button class="favorite styled" value="downvote" type="button">
+            DOWNVOTE
+          </button>
+      </form>
+    </div>
+  </section>
+  `;
+  return $(resource);
+}
+
+//escape function makes text safe and prevents injection
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
