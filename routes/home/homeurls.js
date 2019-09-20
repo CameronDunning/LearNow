c52ab37336b2def514d36fd76b2859b2ac7bcb2e
Michael-Choi <Michael.choi@queensu.ca>
2019-09-20 16:05:17 +0000

    added routing paths

---
+++ routes/home/homeurls.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+
+//import EJS files here login/register/home
+
+/**
+ * Get/post: login page
+ * get/post: register
+ * Get/Post: Create new resource
+ * Get/Post: update profile
+ * Get/post: comment on resource
+ * Get     : my resources
+ *  */
+
+//Get and post requests for the login page
+router.get("login", (req, res) => {
+  res.render("login", templateVars);
+});
+router.post("login", (req, res) => {
+  res.render("login", templateVars);
+});
+
+//Get and post requests for the register page
+router.get("register", (req, res) => {
+  res.render("register", templateVars);
+});
+router.post("register", (req, res) => {
+  res.render("register", templateVars);
+});
+
+//get request for the home page
+router.get("/", (req, res) => {
+  res.render("showresources", templateVars);
+});
