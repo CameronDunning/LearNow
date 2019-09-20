152ff5431398810b514783f4513207f7c04f1e1f
Michael-Choi <Michael.choi@queensu.ca>
2019-09-20 15:30:46 +0000

    setting up standard express server

---
+++ server.js
@@ -0,0 +1,25 @@
+//importing requirements
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+//routes
+
+//middleware
+app.set("view engine", "ejs");
+app.use(
+  cookieSession({
+    name: "Session",
+    keys: [0]
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
