4027f04be1aaeed739bb2ffc4143971321a028e8
cindyhalim <jcindyhalim@gmail.com>
2019-09-20 18:27:55 +0000

    Connect login and register to server

--- routes/home/users/userUrls.js
+++ routes/home/users/userUrls.js
@@ -5,17 +5,17 @@ module.exports = db => {
   //goes to user profile page
   router.get(":user/", (req, res) => {
     let user = req.params.user;
-    res.render("user", templateVars);
+    res.render("user");
   });
 
   //edits users stuff
   router.put(":user/", (req, res) => {
-    res.render("user", templateVars);
+    res.render("user");
   });
 
   //shows user's resources
   router.get(":user/:category", (req, res) => {
-    res.render("user", templateVars);
+    // res.render("user");
   });
 
   return router;
