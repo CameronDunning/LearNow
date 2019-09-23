5b0a79f43904c79eac9ef9bd69707cf15c302c99
Michael-Choi <Michael.choi@queensu.ca>
2019-09-23 17:42:20 +0000

    method override

--- routes/home/users/userUrls.js
+++ routes/home/users/userUrls.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+const bcrypt = require("bcrypt");
 
 module.exports = db => {
   //goes to user profile page
-  router.get(":user/", (req, res) => {
+  router.get("/:user/", (req, res) => {
     let user = req.params.user;
-    res.render("user");
+    templateVars = {
+      userid: user
+    };
+    res.render("update_profile", templateVars);
+  });
+
+  router.put("/u/3/", (req, res) => {
+    console.log("received put");
   });
 
   //edits users stuff
-  router.put(":user/", (req, res) => {
-    res.render("user");
+  router.put("/:user/", (req, res) => {
+    const { name, password } = req.body;
+    console.log("inside put req");
+    let updateQuery = `
+    UPDATE users
+    SET
+      name=$1
+      password=$2
+    WHERE id=$3`;
+
+    updateValues = [name, bcrypt.hashSync(password, 10), req.params.user];
+    db.query(updateQuery, updateValues).then(res.redirect("/"));
   });
 
   //shows user's resources
